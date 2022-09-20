3fa6c151ce3271c9de1b895782d08cb7df69d51b
“Michael <michaelrodiles@gmail.com>
2022-09-20 10:10:25 -0400

    searchParamsDone

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  //   const location = "Seattle, WA";
+  const [location, setLocation] = useState("");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
-          Location
-          <input id="location" value={location} placeholder="Location" />
+          Location 
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
@@ -14,3 +22,6 @@ const SearchParams = () => {
 };
 
 export default SearchParams;
+
+// useState is a Hook anything that starts with use its a hook
+//do not use conditional hooks
